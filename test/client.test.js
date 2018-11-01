a058a49bf97b5ab7f0fab3ff77bd785f01db31a0
Geoff Dutton <g.dutton@gmail.com>
2018-11-01 09:10:51 -0500

    add getEvent and example

--- test/client.test.js
+++ test/client.test.js
@@ -5,6 +5,27 @@ const FomoClient = require('../lib/client')
 const request = require('superagent')
 jest.mock('superagent')
 
+const exampleResponse = {
+  'id': 1,
+  'external_id': 'adf23r',
+  'event_type_id': '123',
+  'url': 'http://fomo.com',
+  'first_name': 'John',
+  'city': 'New York',
+  'country': 'US',
+  'title': 'San Francisco Dealership',
+  'image_url': 'http://newevent.com',
+  'created_at_to_seconds_from_epoch': 1123123123,
+  'message': 'John from New York just got a new Corvette at our San Francisco Dealership',
+  'link': 'http://fomo.com/some-awesome-link?utm_source=fomo&utm_medium=notification',
+  'custom_event_fields_attributes': [
+    {
+      'key': 'model',
+      'value': 'Corvette'
+    }
+  ]
+}
+
 describe('FomoClient', () => {
   let client
 
@@ -25,39 +46,25 @@ describe('FomoClient', () => {
   })
 
   it('POSTs', () => {
-    client.post('/path', { some: 'data' })
+    client._post('/path', { some: 'data' })
     expect(request.post).toHaveBeenCalledWith(`${client.endpoint}/path`)
     expect(request.set).toHaveBeenCalledWith('Authorization', `Token ${client.token}`)
     expect(request.send).toHaveBeenCalledWith({ some: 'data' })
   })
 
+  it('GETs', () => {
+    client._get('/something/3')
+    expect(request.get).toHaveBeenCalledWith(`${client.endpoint}/something/3`)
+    expect(request.set).toHaveBeenCalledWith('Authorization', `Token ${client.token}`)
+  })
+
   describe('routes', () => {
     beforeEach(() => {
-      jest.spyOn(client, 'post')
+      jest.spyOn(client, '_post')
+      request.__setMockResponseBody(exampleResponse)
     })
 
-    it('POSTs an event', () => {
-      request.__setMockResponseBody({
-        'id': 1,
-        'external_id': 'adf23r',
-        'event_type_id': '123',
-        'url': 'http://fomo.com',
-        'first_name': 'John',
-        'city': 'New York',
-        'country': 'US',
-        'title': 'San Francisco Dealership',
-        'image_url': 'http://newevent.com',
-        'created_at_to_seconds_from_epoch': 1123123123,
-        'message': 'John from New York just got a new Corvette at our San Francisco Dealership',
-        'link': 'http://fomo.com/some-awesome-link?utm_source=fomo&utm_medium=notification',
-        'custom_event_fields_attributes': [
-          {
-            'key': 'model',
-            'value': 'Corvette'
-          }
-        ]
-      })
-
+    test('createEvent', () => {
       const event = new FomoEvent({
         'event_type_id': '123',
         'external_id': 'adf23r',
@@ -76,7 +83,16 @@ describe('FomoClient', () => {
 
       return client.createEvent(event)
         .then(savedEvent => {
-          expect(client.post).toHaveBeenCalledWith('/events', { event })
+          expect(client._post).toHaveBeenCalledWith('/events', { event })
+          expect(savedEvent).toBeInstanceOf(FomoEvent)
+        })
+    })
+
+    test('getEvent', () => {
+      client._get = jest.fn().mockResolvedValue(exampleResponse)
+      return client.getEvent(1)
+        .then(savedEvent => {
+          expect(client._get).toHaveBeenCalledWith('/events/1')
           expect(savedEvent).toBeInstanceOf(FomoEvent)
         })
     })
